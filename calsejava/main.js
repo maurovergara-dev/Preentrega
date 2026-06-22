fec9dd2e33b3ba4ef1449c21fe1429aad53dc414
maurovergara-dev <mauro.vergara@hospitalitaliano.org.ar>
2026-06-21 21:45:06 -0300

    Solucionado problema de repositorio anidado en carpeta menu

---
+++ calsejava/main.js
@@ -0,0 +1,20 @@
+
+console.log("esto es texto");
+
+
+console.log('Este es mi nuevo mensaje por consola'):
+
+alert("hola este es un mensajke de alerta");
+
+
+
+var a;
+
+let b;
+
+a = 10;
+
+b = "10";
+
+console.log(a);
+
